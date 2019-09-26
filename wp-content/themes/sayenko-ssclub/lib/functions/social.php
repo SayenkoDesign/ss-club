dcec0a3ff90a820fd6e1cb6a095ab4593b403a1a
Kyle Rumble <kylerumble@gmail.com>
2019-09-26 09:45:30 -0700

    Can the social media links open in a new tab?

--- wp-content/themes/sayenko-ssclub/lib/functions/social.php
+++ wp-content/themes/sayenko-ssclub/lib/functions/social.php
@@ -33,7 +33,7 @@ function _s_get_social_icons( $profiles = array(), $post_id = 'options' ) {
         $icon = get_svg( $type );
         
         if( !empty( $icon ) ) {
-			$out .= sprintf( '<li class="social-icon"><a href="%s" class="%s" title="%s">%s</a></li>', $url, $type, ucwords( $type ), $icon );
+			$out .= sprintf( '<li class="social-icon"><a href="%s" class="%s" title="%s" target="_blank">%s</a></li>', $url, $type, ucwords( $type ), $icon );
 		}
 	}
 	
